c2de45cf9682969e3d7cedd6ea2f00f130ab85b2
Sarah Massengill <sarahmassengill@gmail.com>
2020-12-01 18:00:03 -0800

    adding code and png for plot4, minor fix to plot3

--- plot4.R
+++ plot4.R
@@ -27,16 +27,25 @@ data$DateTime = paste(data$Date,data$Time, sep=" ")
 data$DateTime = dmy_hms(data$DateTime)
 
 ## launch png graphics device
-png(file = "plot3.png")
+png(file = "plot4.png")
 
-## plot the sub_metering_1 with respect to date,
+## set up the parameter so that there will be 4 graphs in a 2x2 formation
+## plots will fill the columns first
+## set margins 
+par(mfcol = c(2,2))
+
+##row 1, col 1: Plot the Global Active power for each day 
+with(data, plot(DateTime,Global_active_power,
+                type = "l", # connect data with lines
+                xlab = "",  # no label for the horizontal axis
+                ylab = "Global Active Power"))
+
+## row 2, col 1: plot the sub_metering_1 with respect to date,
 with(data, plot(DateTime,Sub_metering_1,
                 type = "l", # connect data with lines
                 col = "black", # black lines
                 xlab = "",  # no label for the horizontal axis
-                ylab = "Energy sub metering"
-                )
-     )
+                ylab = "Energy sub metering"))
 
 ## add Sub_metering_2 and Sub_metering_3  to the first plot
 with(data, points(DateTime,Sub_metering_2, type="l", col="red"))
@@ -45,8 +54,19 @@ with(data, points(DateTime,Sub_metering_3, type="l", col='blue'))
 ## add a legend to differentiate between each of the sub_metering curves
 legend('topright', col=c("black", "red", "blue"), 
        legend=c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"),
-       lty = 1) # this uses a lines where as pch uses a symbol.
-       
+       lty = 1,  # this uses a lines where as pch uses a symbol
+       bty = "n")  # don't draw a box around the legend
+
+## row 1, col 2: Plot Voltage for each day. 
+with(data, plot(DateTime,Voltage, 
+                type = "l", 
+                xlab = "datetime"))
+
+## row 2, col 2: Plot global reactive power for each day
+with(data, plot(DateTime, Global_reactive_power, , 
+                type = "l", 
+                xlab = "datetime"))
+
 
 ## close graphics device
 dev.off()
